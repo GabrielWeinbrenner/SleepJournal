29fa8fcf325717f1da33069894c764b588d18c4f
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-13 01:18:18 -0400

    Fitbit implementation and react inital

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
-var sleepData = require('./routes/');
+var sleepData = require('./routes/fitbit');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/sleepdata', sleepData);
+app.use('/fitbit', sleepData);
+
 
 app.listen(3000, function(){
     console.log("Running Server on 3000");
--- server/routes/fitbit.js
+++ server/routes/fitbit.js
@@ -6,22 +6,22 @@ var router = express.Router();
 const FitbitApiClient = require("fitbit-node");
 const client = new FitbitApiClient({
     clientId: "22BGK4",
-    clientSecret: "d36ea1aa94b45cf71782776f05c08e91",
+    clientSecret: "f05786199d2522c941a4b05ad6e66235",
 });
 
 // redirect the user to the Fitbit authorization page
-router.get("/", (req, res) => {
+router.get("/auth", (req, res) => {
     // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
-    res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://127.0.0.1:8080/'));
+    res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://localhost:3000/fitbit/callback'));
 });
 
 // handle the callback from the Fitbit authorization flow
 router.get("/callback", (req, res) => {
     // exchange the authorization code we just received for an access token
-    client.getAccessToken(req.query.code, 'http://127.0.0.1:8080/').then(result => {
+    client.getAccessToken(req.query.code, 'http://localhost:3000/fitbit/callback').then(result => {
         // use the access token to fetch the user's profile information
-        client.get("/profile.json", result.access_token).then(results => {
-            res.send(results[0]);
+        client.get("/sleep/date/2020-04-02/2020-04-08.json", result.access_token).then(results => {
+            res.send(results);
         }).catch(err => {
             res.status(err.status).send(err);
         });
