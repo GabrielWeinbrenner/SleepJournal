f9a715d692abf15e7cf59012a722b1991472fa86
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-16 19:39:15 -0400

    Component Additions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
-    getSleepData(){
-        fetch("http://localhost:3000/fitbit/auth")
-        .then(data => {
-            return data.json();
-        })
-        .then(d => console.log(d));
+    // getSleepData(){
+    //     fetch("http://localhost:3000/fitbit/auth")
+    //     .then(data => {
+    //         return data.json();
+    //     })
+    //     .then(d => console.log(d));
+    // }
+    constructor(){
+        this.state = {
+            currentDate = "04/10/2002",
+        }
     }
     render() {
         return (
             <div>
-                App
+                <HorizontalDates />
+                <div>
+                    <SleepAnalysis />
+                    <SleepLog />
+                </div>
             </div>
         )
     }
---
+++ client/src/components/SleepLog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SleepLog extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SleepLog
